6ebeed49abfe378ac4e6c05304e66ee9f3d12a0a
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-10-27 01:09:11 +0300

    :pencil: to lowercase - math operations

---
+++ Linux/BASH/03_Special-Characters/5.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+var=DslConNNEction
+
+
+echo ${var,} # convert the first character of var to lower case then print
+
+echo ${var,,} # convert all characters of var to lower case then print
---
+++ Linux/BASH/03_Special-Characters/README.md
@@ -0,0 +1,1 @@
+# Special Characters in BASH
